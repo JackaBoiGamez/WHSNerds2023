dca77ddf32ff10e48f3af069931c9ecf4dda7eb0
THE JACK <jackaboipersonal@gmail.com>
2023-10-13 12:36:02 -0500

    Made Information Showcase a Slideshow

---
+++ app.js
@@ -0,0 +1,18 @@
+function changeImage(imgsrc) {
+    document.getElementById('index_foreground_img').src=imgsrc;
+    document.getElementById('index_background_img').src=imgsrc;
+}
+
+classphotos = ["./images/Home/Grades/Senior.jpg", "./images/Home/Grades/Junior.jpg", "./images/Home/Grades/Sophmore.jpg", "./images/Home/Grades/Freshman.jpg"]
+
+let index = 0;
+const imgElement = document.querySelector("#classphoto");
+
+function change() {
+    imgElement.src = classphotos[index];
+    index > 1 ? index = 0 : index++;
+ }
+ 
+ window.onload = function () {
+     setInterval(change, 5000);
+ };
