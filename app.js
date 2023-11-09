b6f979bd53f9051b361e9156a9f297f5d6985db9
JackaBoiGamez <jackrod2006@gmail.com>
2023-11-09 12:39:10 -0600

    Mobile support added

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const imgElement = document.querySelector("#classPhoto");
 function change() {
     imgElement.src = classPhotos[index];
     index > 1 ? index = 0 : index++;
- }
- 
- window.onload = function () {
-     setInterval(change, 5000);
- };
+}
+
+window.onload = function () {
+    setInterval(change, 5000);
+};
+
+// Mobile menu support
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const hiddenElements = document.querySelectorAll('.hidden');
+
+menu.addEventListener('click', function(){
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry)=>{
+    entry.target.classList.toggle('show',entry.isIntersecting)
+  });
+});
+hiddenElements.forEach((el) => observer.observe(el));
